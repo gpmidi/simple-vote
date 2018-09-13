95149406c2624e654d8e2d78b5a1de73f4409538
Paulson McIntyre <pmcintyre@salesforce.com>
2018-09-13 11:28:22 -0400

    Adding /simplevote.properties

--- run.sh
+++ run.sh
@@ -4,6 +4,14 @@ set -x
 
 if [[ -z "${DATABASE_URL}" ]]; then
   export SIMPLEVOTE_DB_URL=$DATABASE_URL
+
+  cat > /simplevote.properties << EOF
+jdbc.url="jdbc:$DATABASE_URL"
+version=1.0.0
+activejdbc.version=1.4.13
+main.class=com.simplevote.webservice.WebService
+EOF
+
 fi
 
 sleep 7s
